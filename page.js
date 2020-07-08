3796592848dbd7839b67fedbb00051ad9e95dfda
Karolina Dys <karolinadyss@gmail.com>
2020-07-08 15:49:58 +0200

    add variables to function fixed code

--- page.js
+++ page.js
@@ -4,16 +4,14 @@ const btn = document.getElementById("button");
 let sing = document.getElementById("sing").value;
 
 function Result() {
-let result;
-if     (sing ==='+')  result = firstNumber + secondNumber;
-else if (sing ==='-') result = firstNumber - secondNumber;
-else if (sing ==='*') result = firstNumber * secondNumber;
-else if (sing ==='/')  result = firstNumber / secondNumber;
-document.getElementById('result').innerHTML = result
-return (result)
-}
-btn.addEventListener("click", function (){
-    Result();
-});
-
-console.log(result)
+    let firstNumber = document.getElementById("firstnumber").value;
+    let secondNumber = document.getElementById("secondnumber").value;   
+    let result;
+    if     (sing ==='+')  result = firstNumber + secondNumber;
+    else if (sing ==='-') result = firstNumber - secondNumber;
+    else if (sing ==='*') result = firstNumber * secondNumber;
+    else if (sing ==='/')  result = firstNumber / secondNumber;
+    document.getElementById('result').innerHTML = result;
+    return (result)
+};
+btn.addEventListener("click", Result)
